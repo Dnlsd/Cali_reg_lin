1f96162c61458dc9774af89196231cf06527d0b3
Daniel Sad <danielsad100@gmail.com>
2025-11-01 11:36:25 -0300

    home v2

--- Home.py
+++ Home.py
@@ -34,25 +34,50 @@ st.title("Previsão de preços de imóveis")
 
 # valores de entrada
 
-longitude = st.number_input('Longitude', value=-122.33)
-latitude = st.number_input('Latitude', value=37.88)
+condados = list(geo['name'].sort_values())
+selecionar_condado = st.selectbox("Condado", condados)
 
-housing_median_age = st.number_input('Idade do imóvel', value=10)
+longitude = geo.query("name == @selecionar_condado")["longitude"].values
+latitude = geo.query("name == @selecionar_condado")["latitude"].values
 
-total_rooms = st.number_input('Total de cômodos', value=800)
-total_bedrooms = st.number_input('Total de quartos', value=100)
-population = st.number_input('População', value=300)
-households = st.number_input('Domicílio', value=100)
+housing_median_age = st.number_input('Idade do imóvel', value=10,
+                                     min_value=0, max_value=50)
 
-median_income = st.slider("Renda média (múltiplos de US$ 10k)", 0.5, 15.0, 4.5, 0.5)
+# total_rooms = st.number_input('Total de cômodos', value=800)
+total_rooms = geo.query("name == @selecionar_condado")["total_rooms"].values
 
-ocean_proximity = st.selectbox("Proximidade do oceano", df['ocean_proximity'].unique())
+# total_bedrooms = st.number_input('Total de quartos', value=100)
+total_bedrooms = geo.query("name == @selecionar_condado")["total_bedrooms"].values
 
-median_income_cat = st.number_input('Categoria de renda', value=4)
+# population = st.number_input('População', value=300)
+population = geo.query("name == @selecionar_condado")["population"].values
 
-rooms_per_household = st.number_input("Quartos por dominílio", value=7)
-bedrooms_per_room = st.number_input("Quartos por cômodo", value=0.2)
-population_per_household = st.number_input("Pessoas por dominílio", value=2)
+# households = st.number_input('Domicílio', value=100)
+households = geo.query("name == @selecionar_condado")["households"].values
+
+median_income = st.slider("Renda média (em milhares de US$)", 
+                          5, 100, 45, 5)
+median_income_10 = median_income / 10
+
+# ocean_proximity = st.selectbox("Proximidade do oceano", df['ocean_proximity'].unique())
+ocean_proximity = geo.query("name == @selecionar_condado")['ocean_proximity'].values
+
+
+# median_income_cat = st.number_input('Categoria de renda', value=4)
+bins_income = [0, 1.5, 3, 4.5, 6, np.inf]
+median_income_cat = np.digitize(median_income_10, 
+                                bins=bins_income)
+
+
+# rooms_per_household = st.number_input("Quartos por dominílio", value=7)
+rooms_per_household = geo.query("name == @selecionar_condado")['rooms_per_household'].values
+
+# bedrooms_per_room = st.number_input("Quartos por cômodo", value=0.2)
+bedrooms_per_room = geo.query("name == @selecionar_condado")['bedrooms_per_room'].values
+
+# population_per_household = st.number_input("Pessoas por dominílio", value=2)
+population_per_household = geo.query("name == @selecionar_condado")['population_per_household'].values
+
 
 entrada_modelo = {
     "longitude": longitude,
@@ -62,7 +87,7 @@ entrada_modelo = {
     "total_bedrooms": total_bedrooms,
     "population": population,
     "households": households,
-    "median_income": median_income,
+    "median_income": median_income_10,
     "ocean_proximity": ocean_proximity,
     "median_income_cat": median_income_cat,
     "rooms_per_household": rooms_per_household,
@@ -77,4 +102,4 @@ botao_previsao = st.button("Prever Preço")
 
 if botao_previsao:
     preco = modelo.predict(df_entrada_modelo)
-    st.write(f"Preço previsot: US$ {preco[0][0]:,.2f}")
+    st.write(f"Preço previsot: US$ {round(preco[0][0], -2):,.0f}")
